3b72eb225eed5cbb75dc7493645da12861a54c21
Horace Williams <horace.d.williams@gmail.com>
2016-06-27 14:36:24 -0600

    parse existing possibilities out of a puzzle file

--- src/robodoku/core.clj
+++ src/robodoku/core.clj
@@ -1,6 +1,22 @@
-(ns robodoku.core)
+(ns robodoku.core
+  (:require [clojure.string :refer [split join]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def alphabet (map (comp str char) (range 65 91)))
+
+(defn lines [file] (-> file slurp (split #"\n")))
+
+(defn cell-possibilities [str puzzle-size]
+  (case str
+    " " (set (range 1 (inc puzzle-size)))
+    #{(Integer/parseInt str)}))
+
+(defn read-puzzle [path]
+  (into {} (mapcat (fn [row row-name]
+                     (map (fn [cell-value row-number]
+                            [(str row-name row-number)
+                             (cell-possibilities (str cell-value)
+                                                 (count row))])
+                          row
+                          (iterate inc 1)))
+                    (lines path)
+                    alphabet)))
--- test/robodoku/core_test.clj
+++ test/robodoku/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [robodoku.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-reading-a-puzzle
+  (let [p (read-puzzle "resources/puzzles/four_by_four.txt")]
+    (is (= #{"A1" "A2" "A3" "A4"
+             "B1" "B2" "B3" "B4"
+             "C1" "C2" "C3" "C4"
+             "D1" "D2" "D3" "D4"}
+           (set (keys p))))
+
+    (is (= #{3} (p "A1")))
+    (is (= #{1 2 3 4} (p "D2")))
+    ))
+
+(run-tests)
